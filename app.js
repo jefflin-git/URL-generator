9906df2e52fb8648a5070a8b600e80084778835e
jefflin <linjef0711@gmail.com>
2020-12-31 15:31:38 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const PORT = 3000
+
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+require('./config/mongoose')
+
+// setting static files
+app.use(express.static('public'))
+
+// setting template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// setting body-parser
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('test')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express is listening on localhost:${PORT}at ${Date()}`)
+})
