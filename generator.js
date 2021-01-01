405e7ad3c1aa312f81f3752c564cf0e86f6ba0c9
jefflin <linjef0711@gmail.com>
2021-01-01 12:17:07 +0800

    feat: create shorten  generator

---
+++ generator.js
@@ -0,0 +1,13 @@
+function getRandomUrl(amount) {
+  const data = 'abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const dataArray = data.split('')
+  let result = ''
+  for (let index = dataArray.length - 1; index > 0; index--) {
+    let randomIndex = Math.floor(Math.random() * (index + 1))
+      ;[dataArray[index], dataArray[randomIndex]] = [dataArray[randomIndex], dataArray[index]]
+  }
+  result = 'https://git.heroku.com/secure-caverns-88178.git/' + dataArray.slice(0, amount).join('')
+  return result
+}
+
+module.exports = getRandomUrl
